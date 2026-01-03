c4191fe5012b3c5bbd3815990ba42637d4a43c8f
pgp <admin@pgpkey.net>
2026-01-03 13:25:31 -0500

    better

--- me.c
+++ me.c
@@ -4,13 +4,7 @@ int main()
 {
     printf("hello! this is me! this is a journal of what happens when i do shit.\n");
     printf("i am writing this in C because i am autistic\n");
-    printf("as much as i love drugs, it must come to an end one day\n");
-    printf("no idea how!\n")
-    system("aplay killingmyself.mp3");
-    printf("did 3000mg of methocarbamol so far, and drank like 6 shots so far\n");
     printf("feeling ok, need weed. mom wont wake up! she has the weed.");
-    printf("i have black dog by samurai blasting in my ears. cyberpunk is a gem.\n");
-    printf("ok now sweet dreams by marilyn manson came on. absolute fucking peak.\n");
     printf("also, this will be compilable. it can be read inside of a terminal! wow!\n");
     printf("fun fact: i have roblox playing! i am fishing.\n");
     printf("hatched pets all day and played pokemmo all day. getting far!\n");
@@ -18,9 +12,9 @@ int main()
   // fyi writing this in C is fun and fundamental to my mind! i must ascend to the 4th dimension with C!
   // i must learn and become a god! hahahhahahahahahhahahahhaha
     printf("i need help lowkey, its getting bad but its ok. my girlfriend saves me everyday. love her!\n");
-    printf("i am at 5000mg of methocarbamol lol feelig NOTHING");
   }
   
     return 0;
 
   }
+

41ce0eff192626edfaaa861308169ed6a4869405
pgp <admin@pgpkey.net>
2026-01-03 03:59:54 -0500

    editing

---
+++ me.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() 
+{
+    printf("hello! this is me! this is a journal of what happens when i do shit.\n");
+    printf("i am writing this in C because i am autistic\n");
+    printf("as much as i love drugs, it must come to an end one day\n");
+    printf("no idea how!\n")
+    system("aplay killingmyself.mp3");
+    printf("did 3000mg of methocarbamol so far, and drank like 6 shots so far\n");
+    printf("feeling ok, need weed. mom wont wake up! she has the weed.");
+    printf("i have black dog by samurai blasting in my ears. cyberpunk is a gem.\n");
+    printf("ok now sweet dreams by marilyn manson came on. absolute fucking peak.\n");
+    
+
+  }
